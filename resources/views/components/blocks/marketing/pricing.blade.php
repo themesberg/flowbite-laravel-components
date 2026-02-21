88f52e262e86c91c4ecea3cac21f29c990309e7c
Dominique Thomas <dthomas@codenco.fr>
2026-02-21 14:32:13 +0100

    feat: wip vérification

--- resources/views/components/blocks/marketing/pricing.blade.php
+++ resources/views/components/blocks/marketing/pricing.blade.php
@@ -3,11 +3,11 @@
     'description' => 'Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.',
 ])
 
-<section class="bg-neutral-primary">
+<section class="bg-white dark:bg-gray-900">
     <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
         <div class="mx-auto max-w-screen-md text-center mb-8 lg:mb-12">
-            <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-heading">{{ $title }}</h2>
-            <p class="mb-5 font-light text-body sm:text-xl">{{ $description }}</p>
+            <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">{{ $title }}</h2>
+            <p class="mb-5 font-light text-gray-500 sm:text-xl dark:text-gray-400">{{ $description }}</p>
         </div>
         <div class="space-y-8 lg:grid lg:grid-cols-3 sm:gap-6 xl:gap-10 lg:space-y-0">
             {{ $slot }}
